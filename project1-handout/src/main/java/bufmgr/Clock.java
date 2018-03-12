2db3d80012d6bb00ed96d68679085e02797246a3
sebastian-Pc\sebastian <seb0465@hotmail.com>
2018-03-12 16:20:14 +0100

    pick victim

--- project1-handout/src/main/java/bufmgr/Clock.java
+++ project1-handout/src/main/java/bufmgr/Clock.java
@@ -1,11 +1,12 @@
 package bufmgr;
 
 public class Clock extends Replacer{
-
-
+        
+        private int current;
+        
 	protected Clock(BufMgr bufmgr) {
 		super(bufmgr);
-		// TODO Auto-generated constructor stub
+		current = 0;
 	}
 
 	@Override
@@ -33,9 +34,29 @@ public class Clock extends Replacer{
 	}
 
 	@Override
+        /**
+         * Looks for a Frame which is elligable for replacement
+         * An elligable frame is a frame which has pincnt 0, but it will not be 
+         * replaced unless its state is also 0, if state is not 0, it is 
+         * decremented
+         * The algorithm uses count to see if an entire cycle of the frametab 
+         * has been looked through, and if it has it can be usemed that no 
+         * elligable frame is to be found 
+         */
 	public int pickVictim() {
-		// TODO Auto-generated method stub
-		return 0;
+            int count = 0;
+		while(count<frametab.length){
+                    if(frametab[current].pincnt == 0){
+                        if(frametab[current].state == 0){
+                            return current;
+                        } else {
+                           frametab[current].state--; 
+                        }
+                    }
+                    count++;
+                    current= (current+1) % (frametab.length - 1);
+                }
+		return -1;
 	}
 
 }
